01a6f5dad987edf301890aef6496498446d32309
Alex <al315562@uji.es>
2018-11-10 14:35:07 +0100

    Add counter and sections

--- js/arena.js
+++ js/arena.js
@@ -3,6 +3,17 @@ $( document ).ready(function() {
   $(function(){
       $("#NavbarIndex").load("./common/Navbar/navbarIndex.html");
       $("#Navbar").load("./common/Navbar/navbar.html");
+
+      var i = 0, howManyTimes = 10000;
+      function f() {
+          $('#counter').text(parseInt($('#counter').text()) + 1);
+          i++;
+          if( i < howManyTimes ){
+              setTimeout( f, 300 );
+          }
+      }
+      f();
+
   });
 
 });
