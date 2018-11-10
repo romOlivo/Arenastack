fa9b0bc174e59542403af5b0a0065a935af59538
Alex <al315562@uji.es>
2018-11-10 12:44:06 +0100

    Appying styles to navbar

--- js/arena.js
+++ js/arena.js
@@ -1,6 +1,7 @@
 $( document ).ready(function() {
 
   $(function(){
+      $("#NavbarIndex").load("./common/Navbar/navbarIndex.html");
       $("#Navbar").load("./common/Navbar/navbar.html");
   });
 
